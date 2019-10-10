baeee219c66afd03e9fb4a2772f01c88ab1dbd59
Syed Zaman Mostafiz <zamanwebdeveloperforssmtech@gmail.com>
2019-10-10 18:12:56 +0600

    7/38 Inventory Management System using PHP, Mysqli, jQuery-Ajax and Bootstrap Part - 5 Learning Completed.

--- public_html/index.php
+++ public_html/index.php
@@ -36,7 +36,7 @@
 		      </div>
 		      
 		      <button type="submit" class="btn btn-primary"><i class="fa fa-lock">&nbsp;</i>Login</button>
-		      <span><a href="#">Register</a></span>
+		      <span><a href="register.php">Register</a></span>
 		    </form>		    
 		  </div>
 		  <div class="card-footer"><a href="#">Forget Password ?</a></div>
